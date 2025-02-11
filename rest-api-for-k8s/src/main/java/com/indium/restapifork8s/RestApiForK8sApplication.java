de95e11097065aea950b81cded818edb0c4875f7
prabhusunderaraman <prabhu.bits@gmail.com>
2025-02-11 11:55:16 +0530

    k8s config

---
+++ rest-api-for-k8s/src/main/java/com/indium/restapifork8s/RestApiForK8sApplication.java
@@ -0,0 +1,13 @@
+package com.indium.restapifork8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiForK8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiForK8sApplication.class, args);
+    }
+
+}
---
+++ rest-api-for-k8s/src/test/java/com/indium/restapifork8s/RestApiForK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indium.restapifork8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiForK8sApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
